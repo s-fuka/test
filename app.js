68c15b2a44d3209d7a00da91dddcaacf3b9d21d3
佐久間達也 <>
2023-04-10 18:39:45 +0900

    ejs追加

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var accountRouter = require('./routes/account');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
+app.listen(8080, () => {});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/account', accountRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'sakumatatsuya' });
+  // res.render('index', { title: 'sakumatatsuya' });
+  res.render('top');
 });
 
 module.exports = router;
