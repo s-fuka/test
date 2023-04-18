e5dd22cb510ee57faf2df98031f6d12e2ba66a1f
fuka.sato@arise.co.jp <fuka.sato@arise.co.jp>
2023-04-18 19:04:06 +0900

    SQL接続文追加

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../connection');
 
 // /* GET home page. */
 router.get('/', function(req, res, next) {
+  connection.connect();
+  console.log(connection)
+  connection.query(
+    'SELECT * FROM tb_user',
+    (error, results) => {
+      console.log(results)
+    }
+  );
+  connection.end();
   // res.render('index', { title: 'sakumatatsuya' });
   res.render('top');
 });
