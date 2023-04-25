441bbe818559c48fefa33dc52e9ae45dddc36fb4
山口陽貴 <>
2023-04-25 21:28:24 +0900

    ログインAPI作成

--- routes/common/messageCode.js
+++ routes/common/messageCode.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
   NOT_SET_ALREADY_SETTING:'すでに登録されているメールアドレスです',
   SAVE_COMPLETED:'アカウント登録が完了しました',
-  SAVE_ERROR:'アカウント登録が失敗しました'
+  SAVE_ERROR:'アカウント登録が失敗しました',
+  LOGIN_ERROR:'ログインに失敗しました'
 });
