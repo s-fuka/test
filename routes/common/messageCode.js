256d276594d347b3b504261f307819be05edba85
fuka.sato@arise.co.jp <fuka.sato@arise.co.jp>
2023-04-26 20:37:10 +0900

    投稿一覧の取得・投稿API作成

--- routes/common/messageCode.js
+++ routes/common/messageCode.js
@@ -2,5 +2,7 @@ module.exports = Object.freeze({
   NOT_SET_ALREADY_SETTING:'すでに登録されているメールアドレスです',
   SAVE_COMPLETED:'アカウント登録が完了しました',
   SAVE_ERROR:'アカウント登録が失敗しました',
-  LOGIN_ERROR:'ログインに失敗しました'
+  LOGIN_ERROR:'ログインに失敗しました',
+  POST_COMPLETED:'投稿が完了しました',
+  POST_ERROR:'投稿に失敗しました'
 });
---
+++ routes/post.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var messageCode = require('./common/messageCode');
+const ApiException = require('./common/Exception');
+const define = require('./common/define');
+var connection = require('../connection');
+
+/**
+ * 投稿一覧取得
+ */
+router.post('/get', (req, res, next) =>{
+  ;(async () => {
+    try {
+        var posts = await getPosts();
+        res.status(200).json({
+          posts : posts
+      });
+    } catch (error) {
+        throw new ApiException(res,messageCode.SAVE_ERROR);
+    }
+  })().catch(next);
+});
+
+/**
+ * 投稿一覧取得
+ */
+router.post('/create', (req, res, next) =>{
+  ;(async () => {
+    try {
+      //後からsessionが持っているuserIdに変更する
+      var userId = 10;
+      await insertPostData(req.body, userId)
+      res.status(200).json({
+          code : messageCode.POST_COMPLETED
+      });
+    } catch (error) {
+        throw new ApiException(res,messageCode.SAVE_ERROR);
+    }
+  })().catch(next);
+});
+
+/**
+ * 投稿全取得
+ */
+async function getPosts() {
+  return new Promise((resolve, reject) => {
+  var sql = 'SELECT T1.id, T1.title, T1.message, T1.post_date, T2.user_name '
+      + 'FROM tb_board AS T1 INNER JOIN tb_user AS T2 '
+      + 'ON T1.user_id = T2.user_id WHERE T1.is_deleted = ?;';
+  var bindData = [define.IS_DELETED_OFF];
+    connection.query(sql, bindData,(err, result) => {
+      return err ? reject(err) : resolve(result);
+    });
+  });
+}
+
+/**
+ * 新規投稿
+  */
+ async function insertPostData(body, userId) {
+
+  var insertData = [userId, body.title, body.message,
+                    new Date(), new Date(), define.IS_DELETED_OFF];
+
+  return new Promise((resolve, reject) => {
+    var insertSql = 'INSERT INTO tb_board( '
+      + 'user_id, title, message, '
+      + 'post_date, modified_date, is_deleted)'
+      + 'values(?, ?, ?, ?, ?, ?)';
+
+    connection.query(insertSql, insertData,(err, result) => {
+      return err ? reject(err) : resolve(result);
+    });
+  });
+}
+
+module.exports = router;
